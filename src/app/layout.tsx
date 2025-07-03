2ec441fe5c8669e8bb6b46b29a430c96d193e597
Erik <graybiller@gmail.com>
2025-07-02 22:01:25 -0400

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>asdf</header>
         {children}
+        <footer>ghjkl</footer>
       </body>
     </html>
   );
