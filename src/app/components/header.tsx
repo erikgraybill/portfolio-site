98f443802b7a67c0588009d8db88c4718b12ba35
Erik <graybiller@gmail.com>
2025-07-05 09:26:02 -0400

    fix to logo, more experimentation, inclusion of color palette

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
     return (
         <div className="border-black/10 border-b h-[60px] flex
         items-center justify-between px-5">
-            <div>
-                <Image src="/EG-Logo.png" alt="logo" width={30} height={30} priority />
-            </div>
+            <Link href={"/"}>
+                <Image src="/EG-Logo.svg" alt="logo" width={40} height={40} priority />
+            </Link>
 
             <ul className="flex items-center gap-3">
                 <li>
