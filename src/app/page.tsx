3a10ad973d7a45bb602eea8d621fa62d7af3070c
Erik <graybiller@gmail.com>
2025-07-13 13:25:05 -0400

    style fixes, added 3rd project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <section id="profile" className="md:relative h-screen">
+      <section id="profile" className="md:relative h-[90vh] md:text-lg">
         <div className="sm:flex block justify-center gap-[5rem] md:pt-30 mx-6">
           <div className="my-auto">
             <Image src="/Erik-headshot-transparent2.png" alt="Headshot" width={400} height={400} priority className="bg-erg-highlight-sage rounded-full" />
@@ -24,10 +24,10 @@ export default function Home() {
                 Let&#39;s talk
               </button>
             </div>
-            <div className="flex justify-center gap-1 py-2">
+            <div className="flex justify-center gap-1 py-3">
               <Image src="/LinkedIn.svg" alt="LinkedIn Icon" width={35} height={35} priority className="hover:cursor-pointer hover:animate-pulse" onClick={() => window.open("https://www.linkedin.com/in/erik-graybill/")}/>
             </div>
-            <p className="md:text-lg">This site is a work in progress...more to come!</p>
+            <p>This site is a work in progress...more to come!</p>
           </div>
         </div>
       <Image src="/Down-Arrow.svg" alt="Down Arrow" width={25} height={25} priority className="invisible md:visible animate-bounce hover:cursor-pointer absolute right-50" onClick={() => location.href="#about"} />
@@ -35,7 +35,7 @@ export default function Home() {
       <section id="about" className="relative h-screen md:text-lg">
         <p className="text-xl pt-12 text-center">Learn More</p>
         <p className="text-5xl text-center font-bold pb-2 text-erg-primary-accent-green">About Me</p>
-        <div className="md:flex block  flex-col">
+        <div className="md:flex block flex-col">
           <div className="md:flex block gap-2 md:mx-auto mx-5 my-2 md:space-x-8">
             <div className="my-5 p-4 flex-[1] border rounded-4xl border-erg-second-accent-tan text-center">
               <div className="flex justify-center mb-2">
@@ -66,13 +66,13 @@ export default function Home() {
       <section id="experience" className="">
         
       </section>
-      <section id="projects" className="relative h-screen md:text-lg md:mb-0 mb-55">
+      <section id="projects" className="relative h-screen md:text-lg md:mb-0 mb-170">
         <p className="text-center pt-12 text-xl">See some of my</p>
         <p className="text-center text-5xl font-bold pb-2 text-erg-primary-accent-green">Projects</p>
         <div className="justify-center">
-          <div className="md:flex block gap-8 mb-8 mt-4 mx-5">
-            <div className="my-5 p-4 flex-[1] border rounded-4xl border-erg-second-accent-tan text-center">
-              <p className="text-2xl font-bold text-erg-primary-accent-green mb-4">Document Splitting and Naming using OCR</p>
+          <div className="md:flex block gap-6 md:space-y-0 space-y-4 mb-8 mt-4 mx-5 h-140 items-stretch">
+            <div className="flex flex-col justify-between flex-1 p-8 border rounded-4xl border-erg-second-accent-tan text-center">
+              <p className="text-2xl font-bold text-erg-primary-accent-green">Document Splitting and Naming using OCR</p>
               <div className="flex flex-wrap flex-row gap-2 justify-around text-left">
                 <p>&#8226;Spearheaded R&D of a document processing system using Azure AI Document Intelligence to extract document names and descriptions from multi-page PDFs.</p>
                 <p>&#8226;Utilized Azure Logic Apps to automate database storage, document splitting and renaming of each document.</p>
@@ -84,12 +84,25 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <div className="my-5 p-4 flex-[1] border rounded-4xl border-erg-second-accent-tan text-center">
-              <p className="text-2xl font-bold text-erg-primary-accent-green mb-4"><span className="">OneStop</span> Application Development</p>
+            <div className="flex flex-col justify-between flex-1 p-8 border rounded-4xl border-erg-second-accent-tan text-center">
+              <p className="text-2xl font-bold text-erg-primary-accent-green"><span className="">OneStop</span> Application Development</p>
               <div className="flex flex-wrap flex-row gap-2 justify-around text-left">
                 <p>&#8226;Led a team in designing and developing a mobile app to improve public transportation usability in State College.</p>
                 <p>&#8226;Applied the design thinking method to interpret use cases, define user needs, and prototype intuitive workflows.</p>
                 <p>&#8226;Built a working prototype using Java in Android Studio, translating wireframes into functional, user-friendly features to be tested by prospective users.</p>
+              </div>
+              <div className="flex justify-center gap-4 mt-8">
+                <button disabled className="bg-transparent border border-erg-primary-accent-green text-erg-text-gray font-bold py-2 px-4 rounded-full opacity-50">
+                  More coming soon...
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between flex-1 p-8 border rounded-4xl border-erg-second-accent-tan text-center">
+              <p className="text-2xl font-bold text-erg-primary-accent-green">Full-Stack Laptop Rental Application</p>
+              <div className="flex flex-wrap flex-row gap-2 justify-around text-left">
+                <p>&#8226;Built a full-stack JavaScript application simulating a laptop rental service.</p>
+                <p>&#8226;Designed and implemented a backend server to manage rental transactions and track inventory.</p>
+                <p>&#8226;Developed a client-side interface to format and validate user inputs and display server responses.</p>
               </div>
               <div className="flex justify-center gap-4 mt-8">
                 <button disabled className="bg-transparent border border-erg-primary-accent-green text-erg-text-gray font-bold py-2 px-4 rounded-full opacity-50">
